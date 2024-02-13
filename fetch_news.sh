269ac590c06982fca774663e84ab75d9f3256d69
ETFrocks <team@etf.rocks>
2024-02-13 22:38:45 +0100

    correct keyword query bugs and set array with value to be replace for alphavantage call.

--- fetch_news.sh
+++ fetch_news.sh
@@ -4,11 +4,11 @@
 BASE_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 # Load API keys from .env file
-source $BASE_DIR/.env
+source $BASE_DIR/.env_private
 
 # Define a list of crypto keywords
-CRYPTO_KEYWORDS=("bitcoin" "ethereum" "uniswap")
-STOCKS_KEYWORDS=("coinbase" "paypal")
+declare -A CRYPTO_KEYWORDS=(["bitcoin"]="btc" ["ethereum"]="eth" ["uniswap"]="uni" ["btc"]="btc" ["eth"]="eth" ["uni"]="uni")
+declare -A STOCKS_KEYWORDS=(["coinbase"]="coin" ["paypal"]="pypl" ["coin"]="coin" ["pypl"]="pypl")
 FOREX_KEYWORDS=("eur" "usd" "jpy" "chf")
 
 # Function to display usage
@@ -24,20 +24,27 @@ usage() {
 
 # Function to fetch news from Alphavantage
 fetch_alphavantage() {
-    keyword=$1
+    local keyword=$1
+
+    # Set default keyword if none provided
+    if [[ "$keyword" == "bitcoin%20OR%20ethereum%20OR%20uniswap%20OR%20coinbase%20OR%20usd" ]]; then
+        keyword="bitcoin"
+    fi
+
     if [ -z "$API_KEY_ALPHAVANTAGE" ]; then
         echo "API_KEY_ALPHAVANTAGE is empty. Skipping fetch from Alphavantage."
         return
     fi
-    if [[ " ${CRYPTO_KEYWORDS[@]} " =~ " ${keyword} " ]]; then
-        keyword="CRYPTO:${keyword^^}"
-    elif [[ " ${STOCKS_KEYWORDS[@]} " =~ " ${keyword} " ]]; then
-        keyword="STOCKS:${keyword^^}"
+    if [[ -n "${CRYPTO_KEYWORDS[$keyword]}" ]]; then
+        keyword="CRYPTO:${CRYPTO_KEYWORDS[$keyword]^^}"
+    elif [[ -n "${STOCKS_KEYWORDS[$keyword]}" ]]; then
+        keyword="STOCKS:${STOCKS_KEYWORDS[$keyword]^^}"
     elif [[ " ${FOREX_KEYWORDS[@]} " =~ " ${keyword} " ]]; then
         keyword="FOREX:${keyword^^}"
     fi
     echo -e "\e[1mFetching news from \e[0;35mAlphavantage\e[0m...\n"
-    echo "https://www.alphavantage.co/query?function=NEWS_SENTIMENT&tickers=${keyword}&time_from=$(date -u -d '1 day ago' +%Y%m%dT%H%M)&apikey=$API_KEY_ALPHAVANTAGE"
+    echo "QUERY: https://www.alphavantage.co/query?function=NEWS_SENTIMENT&tickers=${keyword}&time_from=$(date -u -d '1 day ago' +%Y%m%dT%H%M)&apikey=$API_KEY_ALPHAVANTAGE"
+    echo
     RESPONSE=$(curl -s "https://www.alphavantage.co/query?function=NEWS_SENTIMENT&tickers=${keyword}&time_from=$(date -u -d '1 day ago' +%Y%m%dT%H%M)&apikey=$API_KEY_ALPHAVANTAGE")
     if echo "$RESPONSE" | grep -q "Our standard API rate limit is 25 requests per day. Please subscribe to any of the premium plans"; then
         echo -e "You can't use the free API for this, you need to pay for their minimum subscription. Remove the value in API_KEY_ALPHAVANTAGE in .env file if you don't want to use it.\n"
@@ -54,7 +61,8 @@ fetch_newsapi() {
         return
     fi
     echo -e "\e[1mFetching news from \e[0;35mNewsAPI\e[0m...\n"
-    echo "https://newsapi.org/v2/everything?q=${keyword}&language=en&sortBy=publishedAt&pageSize=3&apiKey=$API_KEY_NEWSAPI_ORG"
+    echo "QUERY: https://newsapi.org/v2/everything?q=${keyword}&language=en&sortBy=publishedAt&pageSize=3&apiKey=$API_KEY_NEWSAPI_ORG"
+    echo
     curl -s "https://newsapi.org/v2/everything?q=${keyword}&language=en&sortBy=publishedAt&pageSize=3&apiKey=$API_KEY_NEWSAPI_ORG" | jq -r '.articles[] | .title, .url, ""'
 }
 
@@ -66,7 +74,8 @@ fetch_gnews() {
         return
     fi
     echo -e "\e[1mFetching news from \e[0;35mGNews\e[0m...\n"
-    echo "https://gnews.io/api/v4/search?q=${keyword}&lang=en&max=3&token=$API_KEY_GNEWS"
+    echo "QUERY: https://gnews.io/api/v4/search?q=${keyword}&lang=en&max=3&token=$API_KEY_GNEWS"
+    echo
     curl -s "https://gnews.io/api/v4/search?q=${keyword}&lang=en&max=3&token=$API_KEY_GNEWS" | jq -r '.articles[] | .title, .url, ""'
 }
 
