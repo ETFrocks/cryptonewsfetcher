7ebf3ffa1dc90ed5748bb8f0ca802f876d39fb97
ETFrocks <team@etf.rocks>
2024-02-13 23:34:08 +0100

    get rid of redundancy in arrayby fixing the alphavantage function

--- fetch_news.sh
+++ fetch_news.sh
@@ -7,10 +7,10 @@ BASE_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 source $BASE_DIR/.env
 
 # Define a list of crypto keywords
-declare -A CRYPTO_KEYWORDS=(["bitcoin"]="btc" ["ethereum"]="eth" ["uniswap"]="uni" ["ripple"]="xrp" ["cardano"]="ada" ["polkadot"]="dot" ["litecoin"]="ltc" ["chainlink"]="link" ["bitcoin cash"]="bch" ["stellar"]="xlm" ["usd coin"]="usdc" ["wrapped bitcoin"]="wbtc" ["aave"]="aave" ["cosmos"]="atom" ["monero"]="xmr" ["eos"]="eos" ["tron"]="trx" ["tezos"]="xtz" ["neo"]="neo" ["nem"]="xem" ["btc"]="btc" ["eth"]="eth" ["uni"]="uni" ["xrp"]="xrp" ["ada"]="ada" ["dot"]="dot" ["ltc"]="ltc" ["link"]="link" ["bch"]="bch" ["xlm"]="xlm" ["usdc"]="usdc" ["wbtc"]="wbtc" ["aave"]="aave" ["atom"]="atom" ["xmr"]="xmr" ["eos"]="eos" ["trx"]="trx" ["xtz"]="xtz" ["neo"]="neo" ["xem"]="xem")
+declare -A CRYPTO_KEYWORDS=(["bitcoin"]="btc" ["ethereum"]="eth" ["uniswap"]="uni" ["ripple"]="xrp" ["cardano"]="ada" ["polkadot"]="dot" ["litecoin"]="ltc" ["chainlink"]="link" ["bitcoin cash"]="bch" ["stellar"]="xlm" ["usd coin"]="usdc" ["wrapped bitcoin"]="wbtc" ["aave"]="aave" ["cosmos"]="atom" ["monero"]="xmr" ["eos"]="eos" ["tron"]="trx" ["tezos"]="xtz" ["neo"]="neo" ["nem"]="xem")
 
 # Define a list of stocks keywords
-declare -A STOCKS_KEYWORDS=(["coinbase"]="coin" ["paypal"]="pypl" ["microsoft"]="msft" ["apple"]="aapl" ["amazon"]="amzn" ["google"]="googl" ["facebook"]="fb" ["tesla"]="tsla" ["nvidia"]="nvda" ["adobe"]="adbe" ["netflix"]="nflx" ["intel"]="intc" ["cisco"]="csco" ["oracle"]="orcl" ["ibm"]="ibm" ["salesforce"]="crm" ["zoom"]="zm" ["shopify"]="shop" ["uber"]="uber" ["lyft"]="lyft" ["slack"]="work" ["twitter"]="twtr" ["square"]="sq" ["atlassian"]="team" ["servicenow"]="now" ["splunk"]="splk" ["fortinet"]="ftnt" ["akamai"]="akam" ["coin"]="coin" ["pypl"]="pypl" ["msft"]="msft" ["aapl"]="aapl" ["amzn"]="amzn" ["googl"]="googl" ["fb"]="fb" ["tsla"]="tsla" ["nvda"]="nvda" ["adbe"]="adbe" ["nflx"]="nflx" ["intc"]="intc" ["csco"]="csco" ["orcl"]="orcl" ["ibm"]="ibm" ["crm"]="crm" ["zm"]="zm" ["shop"]="shop" ["uber"]="uber" ["lyft"]="lyft" ["work"]="work" ["twtr"]="twtr" ["sq"]="sq" ["team"]="team" ["now"]="now" ["splk"]="splk" ["ftnt"]="ftnt" ["akam"]="akam")
+declare -A STOCKS_KEYWORDS=(["coinbase"]="coin" ["paypal"]="pypl" ["microsoft"]="msft" ["apple"]="aapl" ["amazon"]="amzn" ["google"]="googl" ["facebook"]="fb" ["tesla"]="tsla" ["nvidia"]="nvda" ["adobe"]="adbe" ["netflix"]="nflx" ["intel"]="intc" ["cisco"]="csco" ["oracle"]="orcl" ["ibm"]="ibm" ["salesforce"]="crm" ["zoom"]="zm" ["shopify"]="shop" ["uber"]="uber" ["lyft"]="lyft" ["slack"]="work" ["twitter"]="twtr" ["square"]="sq" ["atlassian"]="team" ["servicenow"]="now" ["splunk"]="splk" ["fortinet"]="ftnt" ["akamai"]="akam")
 
 # Define a list of forex keywords
 FOREX_KEYWORDS=("eur" "usd" "jpy" "chf" "gbp" "aud" "cad" "nzd" "sek" "nok" "mxn" "sgd" "hkd" "krw" "try" "rub" "inr" "brl" "zar" "myr" "idr" "thb" "vnd" "php" "egp" "clp" "cop" "ars" "pkr" "bdt")
@@ -39,12 +39,19 @@ fetch_alphavantage() {
         echo "API_KEY_ALPHAVANTAGE is empty. Skipping fetch from Alphavantage."
         return
     fi
-    if [[ -n "${CRYPTO_KEYWORDS[$keyword]}" ]]; then
+
+    # Check if the keyword is already a ticker symbol
+    if [[ " ${CRYPTO_KEYWORDS[@]} " =~ " ${keyword} " ]]; then
+        keyword="CRYPTO:${keyword^^}"
+    elif [[ " ${STOCKS_KEYWORDS[@]} " =~ " ${keyword} " ]]; then
+        keyword="STOCKS:${keyword^^}"
+    elif [[ " ${FOREX_KEYWORDS[@]} " =~ " ${keyword} " ]]; then
+        keyword="FOREX:${keyword^^}"
+    # If not, look it up in the associative array
+    elif [[ -n "${CRYPTO_KEYWORDS[$keyword]}" ]]; then
         keyword="CRYPTO:${CRYPTO_KEYWORDS[$keyword]^^}"
     elif [[ -n "${STOCKS_KEYWORDS[$keyword]}" ]]; then
         keyword="STOCKS:${STOCKS_KEYWORDS[$keyword]^^}"
-    elif [[ " ${FOREX_KEYWORDS[@]} " =~ " ${keyword} " ]]; then
-        keyword="FOREX:${keyword^^}"
     fi
     echo -e "\e[1mFetching news from \e[0;35mAlphavantage\e[0m...\n"
     echo "QUERY: https://www.alphavantage.co/query?function=NEWS_SENTIMENT&tickers=${keyword}&time_from=$(date -u -d '1 day ago' +%Y%m%dT%H%M)&apikey=$API_KEY_ALPHAVANTAGE"
