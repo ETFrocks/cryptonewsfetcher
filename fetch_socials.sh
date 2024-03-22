80adce11b055793b43b35980f22cab662005ee21
ETFrocks <team@etf.rocks>
2024-03-22 23:14:17 +0100

    Add feature to specify social media platforms as arguments

--- fetch_socials.sh
+++ fetch_socials.sh
@@ -3,23 +3,18 @@
 BASE_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 source $BASE_DIR/.env
 
-# Fetch top 3 posts from Instagram
-#bash $BASE_DIR/sub-scripts/instagram.sh
+declare -A platforms
+platforms=( ["instagram"]="instagram.sh" ["facebook"]="facebook.sh" ["linkedin"]="linkedin.sh" ["tiktok"]="tiktok.py" ["reddit"]="reddit.sh" ["twitter"]="twitter.sh" ["youtube"]="youtube.sh" )
 
-# Fetch top 3 posts from Facebook
-#bash $BASE_DIR/sub-scripts/facebook.sh
-
-# Fetch top 3 posts from LinkedIn
-#bash $BASE_DIR/sub-scripts/linkedin.sh
-
-# Fetch top 3 posts from TikTok
-python $BASE_DIR/sub-scripts/tiktok.py
-
-# Fetch top 3 posts from Reddit
-bash $BASE_DIR/sub-scripts/reddit.sh
-
-# Fetch top 3 posts from Twitter
-bash $BASE_DIR/sub-scripts/twitter.sh
-
-# Fetch top 3 posts from YouTube
-#bash $BASE_DIR/sub-scripts/youtube.sh
+for platform in "$@"
+do
+    if [[ -n "${platforms[$platform]}" ]]; then
+        if [[ "${platform}" == "tiktok" ]]; then
+            python $BASE_DIR/sub-scripts/${platforms[$platform]}
+        else
+            bash $BASE_DIR/sub-scripts/${platforms[$platform]}
+        fi
+    else
+        echo "Invalid platform: $platform"
+    fi
+done
